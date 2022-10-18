9ec03066bb8e78820519d39ebb9724f98002be31
MertCetin <mert.cetnn@gmail.com>
2022-10-17 23:13:40 -0400

    schema added

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+   -- Creates Tables
+
+
+   CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    role_id INT NOT NULL
+    PRIMARY KEY (id));
+    
+
+
+)
