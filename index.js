b74353eb21fad713affffa0f07c2459786ef6381
MertCetin <mert.cetnn@gmail.com>
2022-10-17 23:26:51 -0400

    Prompts page added

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTbl = require("console.table");
+
+// THIS PART HELPS US TO CONNECT OUR LOCAL HOST
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3301,
+  user: "root",
+  password: "password",
+  database: "employees_db",
+});
+
+connection.connect((err) => {
+  if (err == true) {
+    throw err;
+  }
+});
+
+function choices() {
+  inquirer.prompt({
+    name: "greeting",
+    type: "list",
+    message: "Welcome to archive ! What is the next move?",
+    choices: [
+      "View all employees",
+      "View all departments",
+      "View all roles",
+      "Add an employee",
+      "Add a department",
+      "Add a role",
+      "Update employee role",
+      "Delete an employee",
+      "EXIT",
+    ],
+  });
+}
