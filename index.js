3410d1b3ebc1e836a24ff0822ede884ee4ef87ef
MertCetin <mert.cetnn@gmail.com>
2022-10-17 23:37:49 -0400

    IF statemnt didnt work try again

--- index.js
+++ index.js
@@ -18,20 +18,39 @@ connection.connect((err) => {
 });
 
 function choices() {
-  inquirer.prompt({
-    name: "greeting",
-    type: "list",
-    message: "Welcome to archive ! What is the next move?",
-    choices: [
-      "View all employees",
-      "View all departments",
-      "View all roles",
-      "Add an employee",
-      "Add a department",
-      "Add a role",
-      "Update employee role",
-      "Delete an employee",
-      "EXIT",
-    ],
-  });
+  inquirer
+    .prompt({
+      name: "greeting",
+      type: "list",
+      message: "Welcome to archive ! What is the next move?",
+      choices: [
+        "View all employees",
+        "View all departments",
+        "View all roles",
+        "Add an employee",
+        "Add a department",
+        "Add a role",
+        "Update employee role",
+        "Delete an employee",
+        "LEAVE",
+      ],
+    })
+    .then(function (respond) {
+      if (message.choices === "View all employees") {
+        showEmployees();}
+        break;
+        if (message.choices === 'View all departments' ) {
+            showDepartmans();}
+            break;
+            if (message.choices ===   'View all roles') {
+                showRoles();}
+                break;
+                if (message.choices ===   'Add an employee') {
+                    addEmployee();}
+                break;
+                
+            
+        
+      
+    );
 }
