678fce1fe5a9c47c525e90c4f5f72fece3169723
MertCetin <mert.cetnn@gmail.com>
2022-10-20 19:17:22 -0400

    try to find rigth path to choices

--- index.js
+++ index.js
@@ -70,109 +70,48 @@ function choices() {
           break;
       }
     });
-}
 
-function showDepartments() {
-  // WHEN FUNCTION CALLED
-  // RUN WISH WICH EQUAL TO MYSQL COMMENT
-  const wish = "SELECT * FROM department";
-  // query() of mysql.Connection THEN USE WISH AND GET RESPOND THEN CHECK ERROR
-  connection.query(wish, function (err, res) {
-    if (err == true) {
-      throw err;
-    }
+  function showDepartments() {
+    // WHEN FUNCTION CALLED
+    // RUN WISH WICH EQUAL TO MYSQL COMMENT
+    const wish = "SELECT * FROM  department";
+    // query() of mysql.Connection THEN USE WISH AND GET RESPOND THEN CHECK ERROR
+    connection.query(wish, function (err, res) {
+      if (err == true) {
+        throw err;
+      }
 
-    console.table("Departments", res);
-    choices();
-  });
-}
+      console.table("Departments", res);
+      choices();
+    });
+  }
 
-function showEmployees() {
-  // WHEN FUNCTION CALLED
-  // RUN WISH WICH EQUAL TO MYSQL COMMENT
-  const wish = "SELECT * FROM employee";
-  // query() of mysql.Connection THEN USE WISH AND GET RESPOND THEN CHECK ERROR
-  connection.query(wish, function (err, res) {
-    if (err == true) {
-      throw err;
-    }
-    // SHOWS OUR RESPONDED LENGTH
-    console.log(res.length + "Here is your team!");
+  function showEmployees() {
+    // WHEN FUNCTION CALLED
+    // RUN WISH WICH EQUAL TO MYSQL COMMENT
+    const wish = "SELECT * FROM employee";
+    // query() of mysql.Connection THEN USE WISH AND GET RESPOND THEN CHECK ERROR
+    connection.query(wish, function (err, res) {
+      if (err == true) {
+        throw err;
+      }
+      // SHOWS OUR RESPONDED LENGTH
+      console.log(res.length + "Here is your team!");
+      //     WILL SHOw Our data about employee
+      console.table("All Employees:", res);
+      choices();
+    });
+  }
 
-    console.table("All Employees:", res);
-    choices();
-  });
-}
-
-// view all roles in the database
-function showRoles() {
-  var wish = "SELECT * FROM role";
-  connection.query(wish, function (err, res) {
-    if (err == true) {
-      throw err;
-    }
-    console.table("All Roles:", res);
-    choices();
-  });
-}
-
-function addEmployee() {
-  connection.query("SELECT * FROM role", function (err, res) {
-    if (err == true) {
-      throw err;
-    }
-    inquirer
-      .prompt([
-        {
-          name: "first_name",
-          type: "input",
-          message: "What is the employee's fist name? ",
-        },
-        {
-          name: "last_name",
-          type: "input",
-          message: "What is the employee's last name? ",
-        },
-        {
-          name: "manager_id",
-          type: "input",
-          message: "What is the employee's manager's ID? ",
-        },
-        {
-          name: "role",
-          type: "list",
-          choices: function () {
-            var roleArray = [];
-            for (let i = 0; i < res.length; i++) {
-              roleArray.push(res[i].title);
-            }
-            return roleArray;
-          },
-          message: "What is this employee's role? ",
-        },
-      ])
-      .then(function (answer) {
-        let role_id;
-        for (let a = 0; a < res.length; a++) {
-          if (res[a].title == answer.role) {
-            role_id = res[a].id;
-            console.log(role_id);
-          }
-        }
-        connection.query(
-          "INSERT INTO employee SET ?",
-          {
-            first_name: answer.first_name,
-            last_name: answer.last_name,
-            manager_id: answer.manager_id,
-            role_id: role_id,
-          },
-          function (err) {
-            if (err) throw err;
-            console.log("Employee added!");
-            options();
-          }
-        );
-      });
-  });
+  // view all roles in the database
+  function showRoles() {
+    var wish = "SELECT * FROM role";
+    connection.query(wish, function (err, res) {
+      if (err == true) {
+        throw err;
+      }
+      console.table("All Roles:", res);
+      choices();
+    });
+  }
 }
